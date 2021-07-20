99d0fb4caae5105ef057862b0ba206ee9d7f3b89
Aarthi Sundaram <55314141+aarsundaram@users.noreply.github.com>
2021-07-20 23:37:14 +0200

    random seed 345 for integ and seg

--- model/households.py
+++ model/households.py
@@ -5,7 +5,7 @@ import random
 from mesa.space import NetworkGrid
 from networkx.classes.function import selfloop_edges
 
-random.seed(234)
+random.seed(345)
 # ---------------------------------------------------------------
 class Household(Agent):
  
